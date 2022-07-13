ead1446c97892f307af90377ddd52f20a300720d
Friturito <74269509+Friturito@users.noreply.github.com>
2022-07-13 14:38:46 -0400

    adding polymorphism.py

---
+++ polymorphism.py
@@ -0,0 +1,21 @@
+class Coche():
+    def desplazamiento(self):
+        print("Me desplazo en cuatro ruedas")
+
+
+class Moto():
+    def desplazamiento(self):
+        print("Me desplazo en dos ruedas")
+
+
+class Camion():
+    def desplazamiento(self):
+        print("Me desplazo en seis ruedas")
+
+
+def desplazamientoVehiculo(vehiculo):
+    vehiculo.desplazamiento()
+
+
+miVehiculo = Camion()
+desplazamientoVehiculo(miVehiculo)
